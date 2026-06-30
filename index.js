822906681f224eb870778b70d652ed4dead799b5
taufeeqhsj <tfqdeadlox46@gmail.com>
2026-06-30 23:49:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 export default {
-  async fetch(request) {
+  async fetch(request, env, ctx) {
     const url = new URL(request.url);
     const targetUrl = `https://api.telegram.org${url.pathname}${url.search}`;
     
+    // Request headers ko safely clone aur modify kiya
     const newHeaders = new Headers(request.headers);
     newHeaders.set('Host', 'api.telegram.org');
     
@@ -12,10 +13,17 @@ export default {
       redirect: 'follow'
     };
     
+    // GET aur HEAD requests ke alawa baaki sabme body pass karenge
     if (request.method !== 'GET' && request.method !== 'HEAD') {
       init.body = request.body;
     }
     
-    return fetch(targetUrl, init);
+    try {
+      const response = await fetch(targetUrl, init);
+      return response;
+    } catch (err) {
+      // Agar Telegram server down ho ya timeout ho toh worker clean response dega
+      return new Response(`Worker Proxy Error: ${err.message}`, { status: 502 });
+    }
   }
 };
